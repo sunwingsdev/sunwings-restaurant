a7b2188d9135688ae4041fd26c9fa149c82e0884
unknown <vismodeb2000@gmail.com>
2024-04-18 14:32:17 +0600

    deshboard modification

--- src/components/home/dashboard/sidebar/Sidebar.jsx
+++ src/components/home/dashboard/sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <div>
       <div className="">
         <ul className="flex gap-2 flex-col p-4">
-          <li className="bg-green-600 text-white py-2 px-4 cursor-pointer hover:bg-green-500 duration-300">
+          <li className="bg-green-600 text-white py-2 px-4 cursor-pointer hover:bg-green-500 hover:text-orange-400 duration-300">
             <Link className="flex gap-2 items-center">
               <MdDashboardCustomize />
               Dashboard
